006c44df0d30bbf5909cda1d0bc7d48eb37711cf
VitorPio7 <vitorvpio60@gmail.com>
2025-04-03 16:19:50 -0300

    today
    today again

--- Eventura/src/pages/Home.jsx
+++ Eventura/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Style from "./css/Home.module.css";
 import { Link } from "react-router";
+import {motion} from 'framer-motion';
 export default function Home(){
     return <main>
         <video className={Style.playVideo} loop muted autoPlay>
@@ -8,7 +9,7 @@ export default function Home(){
         <div className={Style.divText}>
             <h1>Organize share and see events in your concert hall</h1>
             <p>Organize concerts, workshops, and meetups. share your next adventure, connect with communities, and never miss out on local happenings!</p>
-            <Link to="/events"><button className={Style.myButton}>See Events</button></Link>
+            <Link to="/events" ><motion.button  whileHover={{scale:1.2, transition:{type:"spring",bounce:0.7}}} className={Style.myButton}>See Events</motion.button></Link>
         </div>
     </main>
 }
